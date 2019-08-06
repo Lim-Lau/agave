43fe8b1f1d0e3dd6e4c062ded6e1e593a60e40e4
liucan <liucan@chinarecrm.com.cn>
2019-08-06 14:26:01 +0800

    init

---
+++ ans/src/main/java/com/ansimpleasy/agave/ans/AnsApplication.java
@@ -0,0 +1,13 @@
+package com.ansimpleasy.agave.ans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnsApplication.class, args);
+    }
+
+}
