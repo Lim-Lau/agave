14c6b8c0eb69b2a48d9f447de735c4c0aeb47995
liucan <liucan@chinarecrm.com.cn>
2019-08-20 14:14:03 +0800

    #Add:全局异常

---
+++ ans/src/main/java/com/ansimpleasy/agave/ans/exception/AnsException.java
@@ -0,0 +1,19 @@
+package com.ansimpleasy.agave.ans.exception;
+
+/**
+ * @author LiuCan
+ * @date 2019/8/20 9:57
+ */
+public class AnsException extends RuntimeException {
+
+   private static final long serialVersionUID = 1L;
+
+    public AnsException() {
+        super();
+    }
+
+    public AnsException(String msg) {
+        super(msg);
+    }
+
+}
